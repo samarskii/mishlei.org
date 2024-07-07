759d903f87a25331d6470f8c7bf56dd3df70ed59
samarskii <itzhak.samara@gmail.com>
2024-07-07 22:05:01 +0200

    Rewriting translation from beggining

--- chapters/chapters_pl.py
+++ chapters/chapters_pl.py
@@ -1,4 +1,28 @@
 chapters_pl = {
-    "1": {"1": "Przysłowia Salomona, syna Dawida, króla Izraela:", "2": "aby poznać mądrość i karność, aby zrozumieć słowa rozumu;"},
+    "1": {"1": "Przypowieści Salomona, syna Dawida, króla Izraela,", 
+          "2": "Aby poznać mądrość i karność, aby zrozumieć słowa roztropności;",
+          "3": "By przyjąć pouczenie w roztropnym postępowaniu, w sprawiedliwości, prawie i prawości;",
+          "4": "Aby dać prostym roztropność, młodym wiedzę i rozwagę;", 
+          "5": "Mądry posłucha i pomnoży wiedzę, a rozumny nabędzie biegłości w radach;",
+          "6": "Aby rozumieć przypowieść i przenośnię, słowa mędrców i ich zagadki.", 
+          "7": "Bojaźń Pańska początkiem wiedzy; głupcy gardzą mądrością i karnością.",
+          "8": "Słuchaj, synu mój, napomnień ojca i nie odrzucaj nauki matki swojej,",
+          "9": "Bo wdzięcznym wieńcem będą dla twej głowy i naszyjnikiem dla twej szyi.",
+          "10": "Synu mój, jeśli grzesznicy cię nęcą, nie ulegaj im.",
+          "11": 'Jeśli powiedzą: "Chodź z nami! Zaczajmy się na krew, zasadźmy się na niewinnego bez powodu,',
+          "12": "Połknijmy ich żywcem jak Szeol, całych, jak zstępujących do grobu;",
+          "13": "Znajdziemy wszelkie kosztowności, napełnimy nasze domy łupem.",
+          "14": 'Rzuć swój los pośród nas; jedna sakiewka będzie dla nas wszystkich”,',
+          "15": "Synu mój, nie chodź ich drogą; powstrzymaj swą nogę od ich ścieżek,",
+          "16": "Bo ich nogi biegną ku złu i spieszą się, by przelać krew.",
+          "17": "Bo na próżno zastawia się sieć na oczach wszelkiego ptactwa,",
+          "18": "A oni na własną krew czyhają, czatują na własne życie.",
+          "19": "Takie są ścieżki każdego, kto jest chciwy nieuczciwego zysku; on zabiera życie swoim właścicielom.",
+          "20": "Mądrość woła głośno na ulicach, na placach podnosi swój głos;",
+          "21": "Na rogach ulic pełnych zgiełku woła; u wejść do bram miejskich wypowiada swoje słowa:",
+          "22": '"Jak długo, prostaczkowie, będziecie kochać prostactwo? Jak długo szydercy będą się lubować w swoim szydzeniu, a głupcy nienawidzić wiedzy?',
+          
+          
+          },
     # ... other chapters
 }
